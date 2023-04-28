ab08f101dd03bf9ab17c5b7a8da318aa96e1268f
iqra-ali <iqra.ali@student.moringaschool.com>
2023-04-28 04:06:13 +0300

    my tasks

--- README.md
+++ README.md
@@ -59,3 +59,62 @@ are captured in state.
 _When the form is submitted_, add a new task to the list with the text and
 category from the form. For the tests for this feature to pass, you'll need a
 callback prop named `onTaskFormSubmit` that takes a task object as an argument.
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from "react";
+import CategoryFilter from "./CategoryFilter";
+import NewTaskForm from "./NewTaskForm";
+import TaskList from "./TaskList";
+import { CATEGORIES, TASKS } from "../data";
+
+function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleTaskFormSubmit = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const handleTaskDelete = (taskText) => {
+    const filteredTasks = tasks.filter((task) => task.text !== taskText);
+    setTasks(filteredTasks);
+  };
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
+  return (
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleCategorySelect}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
+      <TaskList tasks={filteredTasks} onDeleteTask={handleTaskDelete} />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Task from "./Task";
+import NewTaskForm from "./NewTaskForm";
+import { CATEGORIES } from "../data";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [taskList, setTaskList] = useState(tasks);
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? taskList
+      : taskList.filter((task) => task.category === selectedCategory);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleDeleteTask = (taskText) => {
+    const updatedTasks = taskList.filter((task) => task.text !== taskText);
+    setTaskList(updatedTasks);
+  };
+
+  const handleTaskFormSubmit = (task) => {
+    setTaskList([...taskList, task]);
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      <div className="category-buttons">
+        {["All", ...new Set(tasks.map((task) => task.category))].map(
+          (category) => (
+            <button
+              key={category}
+              className={selectedCategory === category ? "active" : ""}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </button>
+          )
+        )}
+      </div>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
+      {filteredTasks.map((task) => (
+        <Task key={task.text} task={task} onDeleteTask={handleDeleteTask} />
+      ))}
     </div>
   );
 }
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default TaskList;
